9406f9c13ca3edefe8e2a199d6d6e439830318fc
Brian Hopkins <brianhh1230@gmail.com>
2023-02-04 14:18:24 -0500

    feat(init): adding the initial files

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # jasper_twitchchat
 A basic twitch chat connector that redirects information to a rasa instance.
+
+# How to Install Locally
+Ensure you are working out of a python venv:
+
+`python -m venv .venv` to create one
+
+Then you can activate it with `source .venv/bin/activate` if on linux for example.
+
+Now you can install the requirements:
+
+`pip install requirements.txt`
+
+# How To Use Locally
+Now that you have it installed you can use it by filling out the .env from the .env.example information then run the bot via:
+
+From your activated virtual env:
+
+`python bot.py` and you should see some output saying it connected and the user it connected as.
+
+# Testing The Bot
+You can test the bot by going to twitch chat and doing `!hello` and seeing if it responds hello.
---
+++ bot.py
@@ -0,0 +1,53 @@
+from twitchio.ext import commands
+import os
+from dotenv import load_dotenv
+
+import requests
+import json
+
+load_dotenv()
+
+class Bot(commands.Bot):
+
+    def __init__(self):
+        # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
+        super().__init__(token=os.getenv('TOKEN'), client_id=os.getenv('CLIENT_ID'),prefix=os.getenv('BOT_PREFIX'), initial_channels=[os.getenv('CHANNEL')])
+
+    async def event_ready(self):
+        # We are logged in and ready to chat and use commands...
+        print(f'Logged in as | {self.nick}')
+        print(f'User id is | {self.user_id}')
+
+    @commands.command(name='hello')
+    async def hello(self, ctx: commands.Context):
+        print(ctx.author.name)
+        # Send a hello back!
+        await ctx.send(f'Hello {ctx.author.name}!')
+
+    @commands.command(name='jasper')
+    async def jasper(self, ctx: commands.Context,*,message):
+        print(ctx.author.name)
+        print(ctx.__dict__)
+        print(message)
+        jasper_url = os.getenv('RASA_URL')
+        headers = {
+            'Content-Type': 'application/json',
+        }
+
+        json_data = {
+            'sender': ctx.author.name,
+            'message': message,
+        }
+
+        print(json_data)
+
+        response = requests.post(jasper_url, headers=headers, json=json_data)
+        parsed = json.loads(response.content)
+        jasper_response = parsed[0]['text']
+        print(jasper_response)
+        # Send response from Jasper
+        await ctx.send(jasper_response)
+
+
+bot = Bot()
+bot.run()
