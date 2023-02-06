450c2cd56e72aa466c524fb9ce222a2789fde6f9
Brian Hopkins <brianhh1230@gmail.com>
2023-02-05 22:23:22 -0500

    feat(bot): updating bot so it can respond to mentions

--- bot.py
+++ bot.py
@@ -18,6 +18,46 @@ class Bot(commands.Bot):
         print(f'Logged in as | {self.nick}')
         print(f'User id is | {self.user_id}')
 
+    async def event_message(self, message):
+        # Messages with echo set to True are messages sent by the bot...
+        # For now we just want to ignore them...
+        if message.echo:
+            return
+
+        print(message)
+
+        if "@jasperchat" in message.content:
+            print("We would then process this with rasa...")
+            print(message.author.name)
+            print(message.content)
+            print(message.channel)
+            jasper_url = os.getenv('RASA_URL')
+            headers = {
+                'Content-Type': 'application/json',
+            }
+
+            json_data = {
+                'sender': message.author.name,
+                'message': message.content,
+            }
+
+            print(json_data)
+
+            response = requests.post(jasper_url, headers=headers, json=json_data)
+            parsed = json.loads(response.content)
+            print(parsed)
+            jasper_response = parsed[0]['text']
+            print(jasper_response)
+            # Send response from Jasper
+            chan = bot.get_channel(message.channel.name)
+            self.loop.create_task(chan.send(jasper_response))
+            
+       
+        # Since we have commands and are overriding the default `event_message`
+        # We must let the bot know we want to handle and invoke our commands...
+        await self.handle_commands(message)
+
+
     @commands.command(name='hello')
     async def hello(self, ctx: commands.Context):
         print(ctx.author.name)
