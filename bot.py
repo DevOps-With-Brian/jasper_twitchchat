6de0e7dde0eca5f275db9f549fd759963fab4379
Brian Hopkins <brianhh1230@gmail.com>
2023-02-23 20:02:55 -0500

    feat(diffusion-api): adding in the diffusion api calls

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 from twitchio.ext import commands
 import os
 from dotenv import load_dotenv
+import replicate
+
 
 import requests
 import json
@@ -58,11 +60,48 @@ class Bot(commands.Bot):
         await self.handle_commands(message)
 
 
-    @commands.command(name='hello')
-    async def hello(self, ctx: commands.Context):
+    @commands.command(name='diffusion')
+    async def hello(self, ctx: commands.Context,*,message):
         print(ctx.author.name)
-        # Send a hello back!
-        await ctx.send(f'Hello {ctx.author.name}!')
+        
+        model = replicate.models.get("stability-ai/stable-diffusion")
+        version = model.versions.get("db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf")
+
+        # https://replicate.com/stability-ai/stable-diffusion/versions/db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf#input
+        inputs = {
+            # Input prompt
+            'prompt': message,
+
+            # pixel dimensions of output image
+            'image_dimensions': "768x768",
+
+            # Specify things to not see in the output
+            # 'negative_prompt': ...,
+
+            # Number of images to output.
+            # Range: 1 to 4
+            'num_outputs': 1,
+
+            # Number of denoising steps
+            # Range: 1 to 500
+            'num_inference_steps': 50,
+
+            # Scale for classifier-free guidance
+            # Range: 1 to 20
+            'guidance_scale': 7.5,
+
+            # Choose a scheduler.
+            'scheduler': "DPMSolverMultistep",
+
+            # Random seed. Leave blank to randomize the seed
+            # 'seed': ...,
+        }
+
+        # https://replicate.com/stability-ai/stable-diffusion/versions/db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf#output-schema
+        output = version.predict(**inputs)
+
+        # Send Diffusion Pic URL
+        await ctx.send(f'Your stable diffusion image url {ctx.author.name} is {output[0]}')
 
     @commands.command(name='jasper')
     async def jasper(self, ctx: commands.Context,*,message):
